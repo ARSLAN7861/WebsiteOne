4c4f6b0ca3c76688c5a3558f23c0ada3953621ad
Arslan Mumtaz <arslanmumtaz0000@gmail.com>
2024-07-09 18:44:50 +0500

    Done with page 6

--- script.js
+++ script.js
@@ -369,3 +369,26 @@ function canvas1(){
   });
   }
   canvas1()
+
+
+  var clutter = "";
+
+  document.querySelector("#page6>h1").textContent.split(" ").forEach(function(dets){
+      clutter += `<span>${dets}</span> `; 
+  });
+  
+  document.querySelector("#page6>h1").innerHTML = clutter;
+  
+  gsap.to("#page6>h1>span", {
+      scrollTrigger: {
+          trigger: "#page6>h1>span",
+          start: "top bottom",
+          end: "bottom top",
+          scroller: "#main",
+          scrub: .6,
+      },
+      stagger: .2,
+      color: "white"
+  });
+  
+
